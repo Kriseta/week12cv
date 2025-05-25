41a6d109e66e02333bcb3e38dd53b07ed9d24920
Kriseta <kriseta.timmas@tktk.ee>
2025-05-25 19:45:28 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,25 +1,19 @@
-const images = ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG']
+const images = ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG'];
 let i = 1;
 
-const targetImage = document.querySelector ('.avatar img');
+const targetImage = document.querySelector('.avatar img');
 
 targetImage.addEventListener('click', () => {
-
     targetImage.style.opacity = '0';
 
     setTimeout(() => {
-
-        if (i === images.length - 1) {
-            i = 0;
-        }
-
         targetImage.src = 'images/' + images[i];
         i++;
 
+        if (i >= images.length) {
+            i = 0;
+        }
+
         targetImage.style.opacity = '1';
-
     }, 200);
-
-//shift + alt + F
-
-    });
+});
